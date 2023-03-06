ec2cb893e9033ea56181ed0d7095d35cbb099ed4
ali ashraf <ranaaliashraf12@gmail.com>
2023-03-06 18:33:32 +0500

    run exmples

--- examples/main.go
+++ examples/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"os"
+
 	d "github.com/alidevhere/go-fast-downloader"
 )
 
 func main() {
+	//"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 	downloader, err := d.NewConcurrentDownloader(d.Options{
-		Url:                 "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+		Url:                 "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_5MB.mp4",
 		OutputFileName:      "test.mp4",
 		OutputFileDirectory: "./",
 	})
@@ -19,5 +22,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	os.Remove("test.mp4")
 }
